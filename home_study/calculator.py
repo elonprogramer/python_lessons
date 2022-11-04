81c77db5d9f02769487e4cd3ac6b7546ebbf95d6
elonprogramer <elonprogramer2004@gmail.com>
2022-11-04 19:50:34 +0100

    update calculator

--- home_study/calculator.py
+++ home_study/calculator.py
@@ -1,29 +1,25 @@
 import time
-print("Привет")
-time.sleep(1)
-print("Введи первое число")
-first_num = int(input())
-print("Введи второе число")
-second_num = int(input())
-print("Введи знак")
-funk = input("плюс минус умножить подилить степень:")
-
-if funk == "плюс":
-	result = first_num + second_num
-	print(f"ответ {first_num}+{second_num}: {result}")
-
-elif funk == "минус":
-	result = first_num - second_num
-	print(f"ответ {first_num}-{second_num}: {result}")
-
-elif funk == "умножить":
-	result = first_num * second_num
-	print(f"ответ {first_num}*{second_num}: {result}")
-
-elif funk == "подилить":
-	result = first_num / second_num
-	print(f"ответ {first_num}/{second_num}: {result}")
-
-elif funk == "степень":
-	result = first_num ** second_num
-	print(f"ответ {first_num}**{second_num}: {result}")
+while :
+	print("Привет")
+	time.sleep(1)
+	print("Введи первое число")
+	first_num = int(input())
+	print("Введи второе число")
+	second_num = int(input())
+	print("Введи знак")
+	funk = input("плюс минус умножить подилить степень:")
+	if funk == "плюс":
+		result = first_num + second_num
+		print(f"ответ {first_num}+{second_num}: {result}")
+	elif funk == "минус":
+		result = first_num - second_num
+		print(f"ответ {first_num}-{second_num}: {result}")
+	elif funk == "умножить":
+		result = first_num * second_num
+		print(f"ответ {first_num}*{second_num}: {result}")
+	elif funk == "подилить":
+		result = first_num / second_num
+		print(f"ответ {first_num}/{second_num}: {result}")
+	elif funk == "степень":
+		result = first_num ** second_num
+		print(f"ответ {first_num}**{second_num}: {result}")
