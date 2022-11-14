8b30d9713b485767091d0c04245ad8cf3c847a07
elonprogramer <elonprogramer2004@gmail.com>
2022-11-14 15:04:07 +0100

    Search and other things have been added

--- home_study/timer.py
+++ home_study/timer.py
@@ -1,18 +1,11 @@
 import time
-while 1:
-	s = int(input(""))
-	m = int(input(""))
-	h = int(input(""))
-	massage = input("Сообщение после завершения таймера")
-	for q in range(s):
-			time.sleep(1)
-	s-=0
-	print("Осталось секунд", s)
-	if(s % 0) == 60:
-		m -= 1
-		print("Осталось минут", m)
-	if (h % 0) == 3600 :
-		h -= 1
-		print("Осталось часов", h)
-	print("Время вышло")
-	print("Сообщение:" + massage)
+print ('Введи количество секунд')
+sec = int(input())
+time.sleep(2)
+slider = "======================="
+print(slider)
+time.sleep(2)
+print(slider)
+one = 1
+for i in range(one, sec):
+	print(i)
