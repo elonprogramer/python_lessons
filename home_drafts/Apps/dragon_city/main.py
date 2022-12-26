000113f5fbde4eb54b2bdcc5e07e75428cc8f59f
ALex <tier1985@gmail.com>
2022-12-26 12:17:09 +0300

    dragon city improvments and fixes

--- home_drafts/Apps/dragon_city/main.py
+++ home_drafts/Apps/dragon_city/main.py
@@ -1,5 +1,5 @@
 import moduls
-moduls.start() #Приветствие игрока
+moduls.start()  # Приветствие игрока
 print("Войти = 1 Зарегистрироваться = 2 Гость = 3")
 game_mode = int(input())
 if game_mode == 1:
@@ -7,12 +7,12 @@ if game_mode == 1:
 elif game_mode == 2:
     moduls.signup()
 elif game_mode == 3:
-    print("В розроботке")
+    print("В разработке")
     print("Хотите зарегистрироваться? что-бы сыграть")
     user_game_input = input("y or n")
     if user_game_input == "y" or user_game_input == "д":
         moduls.sigin()
     else:
-        print("Ошыбка")
+        print("Ошибка")
 else:
-    print("Ошыбка")
+    print("Ошибка")
--- home_drafts/Apps/dragon_city/moduls.py
+++ home_drafts/Apps/dragon_city/moduls.py
@@ -1,11 +1,14 @@
 import time
 
+
 def start():
     print("Привет. в прошлой части ты обокрал страну драконов")
     time.sleep(1)
     print("Пока ты тратил свои деньги . Драконы подали в суд, и выиграли дело")
     time.sleep(1)
-    print("Твое наказание - Востановить економику страны драконов!")
+    print("Твое наказание - Восcтановить экономику страны драконов!")
+
+
 def signup():
     user_login = input("Введи имя: ")
     user_password = input("Введи пароль: ")
@@ -17,20 +20,47 @@ def signup():
             second_password = input("Повтори пароль: ")
         elif user_password == second_password:
             while_pass = False
-    id = 0
-    id = id + 1
-    user_data = str(f"id = {id} \n name = {user_login} \n password = {second_password}")
-    file = open("/home/elon/python_lessons/home_drafts/Apps/dragon_city/config.txt", "a")
+    user_id = 0
+    user_id = user_id + 1
+    # user_data = str(f"id = {id} \n name = {user_login} \n password = {second_password}")
+
+    user_data = str(f"{user_id} {user_login} {second_password}\n")
+    # file = open("/home/elon/python_lessons/home_drafts/Apps/dragon_city/logins.txt", "a")
+    file = open("./logins.txt", "a")
+
     file.write(user_data)
     file.close()
+
 
 def login():
     user_login = input("Введи имя: ")
     user_password = str(input("Введи пароль: "))
-    file = open("/home/elon/python_lessons/home_drafts/Apps/dragon_city/config.txt", "r")
-    if user_login in file and user_password in file:
-        print(f"Поздравляю, {user_login}, Ты успешно авторизован!")
-    elif user_login not in file:
-        print("Ты ввёл не правельный логин")
-    elif user_password not in file:
-        print("Ты ввёл не правельный Пароль")
+    # file = open("/home/elon/python_lessons/home_drafts/Apps/dragon_city/logins.txt", "r")
+    file_logins = open("./logins.txt", "r")
+    login_pass_dict = {}
+
+    user_logins_list = file_logins.read().splitlines()
+    for line in user_logins_list:
+        print(f'---{line}---')
+        fields = line.split(' ')
+        print(f'name = {fields[1]}')
+        login_pass_dict[fields[1]] = fields[2]
+
+    print(login_pass_dict)
+
+    if user_login in login_pass_dict.keys():
+        print(f'WE HAVE USER {user_login}')
+        if user_password == login_pass_dict[user_login]:
+            print(f'Авторизация прошла успешно {user_login}')
+        else:
+            print('Пароль неверный')
+
+    else:
+        print(f'NO SUCH USER {user_login}')
+
+    # if user_login in file and user_password in file:
+    #     print(f"Поздравляю, {user_login}, Ты успешно авторизован!")
+    # elif user_login not in file:
+    #     print("Ты ввёл не правильный логин")
+    # elif user_password not in file:
+    #     print("Ты ввёл не правильный Пароль")
