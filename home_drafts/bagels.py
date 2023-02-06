f47882f01d858bdec8929832236cd75b3fcb5858
elonprogramer <elonprogramer2004@gmail.com>
2023-02-06 17:49:17 +0100

    fixed bangles

--- home_drafts/bagels.py
+++ home_drafts/bagels.py
@@ -1,68 +1,72 @@
 import random
 
-DEBUG_MODE = 1
-MAX_INPUT = 3
+NUM_DIGITS = 3
+MAX_GUESS = 10
+
+def getSecretNum():
+    # Возвращает строку уникальных случайных цифр, длина которой составляет NUM_DIGITS.
+    numbers = list(range(10))
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
+
+def getClues(guess, secretNum):
+    # Возвращает строку с подсказками пользователю "Тепло", "Горячо" и "Холодно".
+    if guess == secretNum:
+        return 'Вы угадали!'
+
+    clues = []
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            clues.append('Горячо')
+        elif guess[i] in secretNum:
+            clues.append('Тепло')
+    if len(clues) == 0:
+        return 'Холодно'
+
+    clues.sort()
+    return ' '.join(clues)
+
+def isOnlyDigits(num):
+    # Возвращает значение True, если num - строка, состоящая только из цифр. В противном случае возвращает False.
+    if num == '':
+        return False
+
+    for i in num:
+        if i not in '0 1 2 3 4 5 6 7 8 9'.split():
+            return False
+
+    return True
 
 
-def debug_print(str_in):
-    if DEBUG_MODE == 1:
-        print(str_in)
-
-letters = "1234567890"
-# letters = list(letters)
-# lenlist = len(letters)
-letters_choice = ""
-
-
-while len(letters_choice) != 3:
-    letters_choice += random.choice(letters)
-
-letters_choice = "147"
-
-debug_print(f'{letters_choice=}')
-letters_choice_list = list(letters_choice)
-# print(letters_choice_list)
-
-print('Я загадаю %s-х значное число, которое вы должны отгадать.' % (MAX_INPUT))
+print('Я загадаю %s-х значное число, которое вы должны отгадать.' % (NUM_DIGITS))
 print('Я дам несколько подсказок...')
-print('Когда я говорю:Это означает:')
-print(' Холодно Ни одна цифра не отгадана.')
-print(' Тепло Одна цифра отгадана, но не отгадана ее позиция.')
-print(' Горячо Одна цифра и ее позиция отгаданы.')
+print('Когда я говорю:    Это означает:')
+print('  Холодно          Ни одна цифра не отгадана.')
+print('  Тепло            Одна цифра отгадана, но не отгадана ее позиция.')
+print('  Горячо           Одна цифра и ее позиция отгаданы.')
 
-MAX_GUESS = 5
-game_guess = 5
-game_true = True
-print(f'Итак, я загадал число. У вас есть {MAX_GUESS} попыток, чтобы отгадать его.')
+while True:
+    secretNum = getSecretNum()
+    print('Итак, я загадал число. У вас есть %s попыток, чтобы отгадать его.' % (MAX_GUESS))
 
+    guessesTaken = 1
+    while guessesTaken <= MAX_GUESS:
+        guess = ''
+        while len(guess) != NUM_DIGITS or not isOnlyDigits(guess):
+            print('Попытка №%s: ' % (guessesTaken))
+            guess = input()
 
-while game_true:
-    game_guess = game_guess - 1
-    user_input = input("Введи 3 Цифры: ")
+        print(getClues(guess, secretNum))
+        guessesTaken += 1
 
-    user_input_answer = list(user_input)
-    user_input_length = len(user_input)
+        if guess == secretNum:
+            break
+        if guessesTaken > MAX_GUESS:
+            print('Попыток больше не осталось. Я загадал число %s.' % (secretNum))
 
-    if user_input_length > MAX_INPUT:
-        print("Ты ввел больше цифр")
-    elif user_input_length < MAX_INPUT:
-        print("Ты ввел меньше цифр")
-    elif user_input_length == MAX_INPUT:
-        if user_input_answer == letters_choice_list:
-            print("Горячо")
-            print("Игра выиграна")
-            game_true = False
-            game_guess = game_guess + 1
-
-        elif user_input_answer != letters_choice_list:
-            for i in letters_choice_list:
-                if i in user_input_answer:
-                    print(i)
-                    # print('\n\n')
-                    # print("тепло")
-                else:
-                    print("Холодно")
-
-    if game_guess == 0:
-        print("Игра проиграна")
-        game_true = False
+    print('Хотите сыграть еще раз? (да или нет)')
+    if not input().lower().startswith('д'):
+        break
