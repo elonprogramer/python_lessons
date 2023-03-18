66cceb0785a83f53e0272f0d008307d79d24726f
elonprogramer <elonprogramer2004@gmail.com>
2023-03-18 10:34:55 +0100

    added pygame

--- pygame/ex1/main.py
+++
@@ -1,9 +0,0 @@
-import pygame
-pygame.init()
-pygame.display.set_mode((1000, 1000))
-pygame.display.set_caption("Влад списує з гдз")
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pass
