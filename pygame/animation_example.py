6f25c3a8214b376746290f87df240d6eaa7c67a8
elonprogramer <elonprogramer2004@gmail.com>
2023-04-08 12:02:39 +0200

    added new game

--- pygame/animation_example.py
+++ pygame/animation_example.py
@@ -2,6 +2,7 @@
 import sys
 
 import pygame
+import random
 
 screenWidth = 600
 # Высота окна
@@ -21,17 +22,38 @@ clock = pygame.time.Clock()
 posX = 20
 posY = 50
 
-speed = 2
-
-
+speed = 5
+speedY = 5
+ball_radius = 20
+c_speed = 5
+c_one = 10
 
 while True:
     screen.fill(BLACK)
     # Назначим FPS
-    clock.tick(25)
-    pygame.draw.circle(screen, GREEN, (posX, posY), 10)
-    posX =posX + speed
-
+    clock.tick(20)
+    c_one = c_one + c_speed
+    print(c_one)
+    if c_one > 240:
+        c_speed = -c_speed
+    elif c_one < 10:
+        c_speed = -c_speed
+    pygame.draw.circle(screen, (c_one, c_one, c_one), (posX, posY), ball_radius)
+    posX = posX + speed
+    posY = posY + speedY
+    if posX > screenWidth - ball_radius:
+        speed = -speed
+    elif posX < ball_radius:
+        speed = -speed
+    else:
+        posY = posY + speedY
+    if posY > screenHeight - ball_radius:
+        speedY = -speedY
+    elif posY < ball_radius:
+        speedY = -speedY
+    #else:
+    #    speed = 4
+    #    posX = posX - speed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
