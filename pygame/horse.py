b7ea930bbfdbc0fbd6bc9852c699b353e4b512ac
elonprogramer <elonprogramer2004@gmail.com>
2023-04-15 12:43:02 +0200

    added object wall horse.py

--- pygame/horse.py
+++ pygame/horse.py
@@ -1,10 +1,11 @@
 # Ширина окна
 import os
 import pygame
-
+from sys import exit as exit
 # Импортируем  папку с картинками
 images_folder = './images'
 horseStand = pygame.image.load(os.path.join(images_folder, 'horse_stand.png'))
+horseJump = pygame.image.load(os.path.join(images_folder, 'horse_jump.png'))
 screenWidth = 800
 # Высота окна
 screenHeight = 600
@@ -12,6 +13,19 @@ screenHeight = 600
 BLACK = (0, 0, 0)
 GREEN = (0, 103, 71)
 BROWN = (205, 127, 50)
+
+
+class Wall(object):
+    def __init__(self, x, y, color, height, speed):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.height = height
+        self.width = 10
+        self.speed = speed
+
+
+black_big_wall = Wall(x=0, y=550, color=BLACK, height=75, speed=5)
 
 pygame.init()
 
@@ -22,8 +36,8 @@ print('window started')
 # Таймер
 clock = pygame.time.Clock()
 
-posX = 0
-posY = 50
+# wallX = 0
+# wallY = 550
 
 speed = 5
 
@@ -31,13 +45,21 @@ while True:
     screen.fill(GREEN)
     # Назначим FPS
     clock.tick(25)
-    posX = posX + speed
-    if posX > screenWidth:
-        posX = 0
-    horseY = screenHeight / 2
+    black_big_wall.x = black_big_wall.x + speed
+    if black_big_wall.x > screenWidth:
+        black_big_wall.x = 0
+    horseY = 550
     horseX = int(screenWidth / 2)
-    screen.blit(horseStand, (horseX, horseY))
-    pygame.draw.rect(screen, BROWN, pygame.Rect(posX, 500, 10, 100))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        horseY = horseY - 55
+        screen.blit(horseJump, (horseX, horseY))
+    else:
+        if horseY != 550:
+            horseY = 550
+        screen.blit(horseStand, (horseX, horseY))
+    print(horseY)
+    pygame.draw.rect(screen, black_big_wall.color, pygame.Rect(black_big_wall.x, black_big_wall.y, black_big_wall.width, black_big_wall.height))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
