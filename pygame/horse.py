407c0fe03701507a4a76ac8ca782c01a063f6aaa
ALex <tier1985@gmail.com>
2023-04-29 11:24:30 +0200

    merge horse game

--- pygame/horse.py
+++ pygame/horse.py
@@ -12,10 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent
 images_folder = (os.path.join(BASE_DIR, 'images'))
 music_folder = (os.path.join(BASE_DIR, 'sounds'))
 
-pygame.mixer.init()
+
 horseStand = pygame.image.load(os.path.join(images_folder, 'horse_stand.png'))
 horseJump = pygame.image.load(os.path.join(images_folder, 'horse_jump.png'))
-pygame.mixer.music.load(os.path.join(music_folder, 'horse_topot.mp3'))
+horseFall = pygame.transform.rotate(horseStand, 180)
+
 screenWidth = 800
 # Высота окна
 screenHeight = 600
@@ -25,6 +26,7 @@ GREEN = (0, 103, 71)
 BROWN = (205, 127, 50)
 YELLOW = (255, 215, 0)
 
+HORIZONT_LEVEL = 550
 
 class Wall(object):
     def __init__(self, x, y, color, height, speed):
@@ -36,9 +38,18 @@ class Wall(object):
         self.speed = speed
 
 
-black_big_wall = Wall(x=0, y=550, color=BLACK, height=75, speed=5)
-brown_small_wall = Wall(x=500, y=550, color=BROWN, height=50, speed=5)
-yellow_small_wall = Wall(x=screenWidth - 100, y=450, color=YELLOW, height=50, speed=5)
+class Horse(object):
+    def __init__(self, x, y, state):
+        self.x = x
+        self.y = y
+        self.state = state
+
+black_big_wall = Wall(x=0, y=HORIZONT_LEVEL, color=BLACK, height=75, speed=3)
+brown_small_wall = Wall(x=screenWidth - 200, y=HORIZONT_LEVEL, color=BROWN, height=50, speed=3)
+yellow_small_wall = Wall(x=screenWidth, y=HORIZONT_LEVEL - 100, color=YELLOW, height=50, speed=5)
+
+black_horse = Horse(int(screenWidth / 2), 550, horseStand)
+
 pygame.init()
 
 # Окно
@@ -56,37 +67,43 @@ clock = pygame.time.Clock()
 # wallY = 550
 
 speed = 5
-pygame.mixer.music.play(1000, 0)
+yellow_small_wall_speed = 6
 
+# music
+pygame.mixer.init()
+pygame.mixer.music.load(os.path.join(music_folder, 'horse_topot.mp3'))
+pygame.mixer.music.play(1000, 0)
 
 while True:
     screen.fill(GREEN)
     # Назначим FPS
-    clock.tick(25)
-    black_big_wall.x = black_big_wall.x + speed
+    clock.tick(60)
+    black_big_wall.x = black_big_wall.x + black_big_wall.speed
     if black_big_wall.x > screenWidth:
         black_big_wall.x = 0
     brown_small_wall.x = brown_small_wall.x + speed
     if brown_small_wall.x > screenWidth:
         brown_small_wall.x = 0
-    yellow_small_wall.x = yellow_small_wall.x - yellow_small_wall.speed
+    yellow_small_wall.x = yellow_small_wall.x - yellow_small_wall_speed
     if yellow_small_wall.x < 0:
         yellow_small_wall.x = screenWidth
-    horseY = 550
-    horseX = int(screenWidth / 2)
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        horseY = horseY - 100
-        screen.blit(horseJump, (horseX, horseY))
+        black_horse.y = HORIZONT_LEVEL - 100
+        print(f"{black_horse.y=}")
+        black_horse.state = horseJump
+        screen.blit(black_horse.state, (black_horse.x, black_horse.y))
         pygame.mixer.music.pause()
     else:
-        if horseY != 550:
-            horseY = 550
+        if black_horse.y != 550:
+            black_horse.y = 550
+        black_horse.state = horseStand
+        screen.blit(black_horse.state, (black_horse.x, black_horse.y))
         pygame.mixer.music.unpause()
-        screen.blit(horseStand, (horseX, horseY))
+
     pygame.draw.rect(screen, black_big_wall.color, pygame.Rect(black_big_wall.x, black_big_wall.y, black_big_wall.width, black_big_wall.height))
-    pygame.draw.rect(screen, brown_small_wall.color, pygame.Rect(brown_small_wall.x, brown_small_wall.y, brown_small_wall.width, brown_small_wall.height))
-    pygame.draw.rect(screen, yellow_small_wall.color, pygame.Rect(yellow_small_wall.x, yellow_small_wall.y, yellow_small_wall.width, yellow_small_wall.height))
+    # pygame.draw.rect(screen, brown_small_wall.color, pygame.Rect(brown_small_wall.x, brown_small_wall.y, brown_small_wall.width, brown_small_wall.height))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
